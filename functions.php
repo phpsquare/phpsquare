054f03a630bc51cd9be0d728713aae04fb2b0261
phpsquare <phpsquare@gmail.com>
2022-05-11 17:54:21 +0530

    Action hook
    Action hook in functions.php of theme

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
-// If you want to change the product title output 
+// Filter hook - If you want to change the product title output 
 function new_product_title( $name, $instance ) { 
     // You can apply your logic here 
     return $name; 
-}; 
-         
+};      
 // add the filter 
 add_filter( 'woocommerce_product_title', 'new_product_title', 10, 2 ); 
+
+
+// Action hook
+function new_footer( ) {
+	echo "Welcome to this website!";
+}
+add_action( 'wp_footer', 'new_footer' );
 
 ?>
